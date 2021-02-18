ee699a11867e23b5c35ba52f55eaccef9a407b2e
Peter Wooden <peter.r.wooden@gmail.com>
2021-02-18 22:46:57 +0000

    add copy all button, and refactor

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useMemo } from 'react';
 import StateContext from '../contexts/StateContext';
 import { formatBoxToString } from '../utils';
-import { Clipboard, X } from 'react-feather';
+import { Copy, X } from 'react-feather';
 import copy from 'copy-to-clipboard';
 
 export default function Sidebar() {
-    const { state: { current: state }, dispatch } = useContext(StateContext);
+    const {
+        state: { current: state },
+        dispatch,
+    } = useContext(StateContext);
 
-    const list = state.boxes.map(box => formatBoxToString(box, state.format));
+    const list = useMemo(
+        () => state.boxes.map((box) => formatBoxToString(box, state.format)),
+        [state.boxes, state.format]
+    );
+    const copyAll = useCallback(() => copy(list.join('\n')), [list]);
+    const copyOne = useCallback(
+        (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            copy(list[+(ev.currentTarget.dataset.index as string)]),
+        [list]
+    );
+    const deleteOne = useCallback(
+        (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+            dispatch({ type: 'CREATE_UNDO_POINT' });
+            dispatch({
+                type: 'REMOVE_BOX',
+                index: +(ev.currentTarget.dataset.index as string),
+            });
+        },
+        [dispatch]
+    );
 
     return (
         <div className="sidebar border-right">
-            <h4>Boxes</h4>
+            <h4 className="d-flex align-items-center justify-content-center">
+                Boxes{' '}
+                <button
+                    type="button"
+                    className="box-list-item-icon ml-2"
+                    onClick={copyAll}
+                    title="Copy"
+                    disabled={list.length === 0}
+                >
+                    <Copy size={20} />
+                </button>
+            </h4>
             <div className="format-area">
                 <span
                     title="Use the variables $tag, $w, $h, $x1, $y1, $y2, $x2"
@@ -44,18 +77,17 @@ export default function Sidebar() {
                             <button
                                 type="button"
                                 className="box-list-item-icon"
-                                onClick={() => copy(list[i])}
+                                data-index={i}
+                                onClick={copyOne}
                                 title="Copy"
                             >
-                                <Clipboard size={16} />
+                                <Copy size={16} />
                             </button>
                             <button
                                 type="button"
                                 className="box-list-item-icon"
-                                onClick={() => {
-                                    dispatch({ type: 'CREATE_UNDO_POINT' });
-                                    dispatch({ type: 'REMOVE_BOX', index: i });
-                                }}
+                                data-index={i}
+                                onClick={deleteOne}
                                 title="Delete"
                             >
                                 <X size={16} />
@@ -70,7 +102,14 @@ export default function Sidebar() {
                     className="d-flex justify-content-center align-items-center p-2 mb-2 rounded"
                 >
                     <span>
-                        {formatBoxToString({ box: state.nextBox, color: state.nextColor, tag: 'Tag'}, state.format)}
+                        {formatBoxToString(
+                            {
+                                box: state.nextBox,
+                                color: state.nextColor,
+                                tag: 'Tag',
+                            },
+                            state.format
+                        )}
                     </span>
                 </div>
             )}
